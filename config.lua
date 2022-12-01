9d5f33f62e6b9e69455651eb90ee4ec977028749
Mr. K V <36492979+69u@users.noreply.github.com>
2022-12-01 08:08:50 +0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,4 +2,4 @@ config = {}
 
 config.identity = false         --If you are using esx_identity then make it true to get rp names on chat
 
-config.DiscordWebhook = 'https://discord.com/api/webhooks/xxxxxxxxxxxxxxxxxxx/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+config.DiscordWebhook = false -- Set to your Discord Webhook URL (https://discord.com/api/webhooks/xxxxxxxxxxxxxxxxxxx/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx)
