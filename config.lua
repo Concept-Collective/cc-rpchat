9f3c534c50cebc6daef502231be4db9f0ec41f7a
Mr. K V <36492979+69u@users.noreply.github.com>
2022-12-20 08:10:16 +0800

    false by default

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 config = {}
 
-config.esx = true                        -- If you are using esx this will enable player names (esx_identity required)
+config.esx = false                        -- If you are using esx this will enable player names (esx_identity required)
 
 config.connectionMessages = true         -- set this to true if you would like join and leave messages
 
