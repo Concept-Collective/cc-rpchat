8a4073b835457b4026f2304b95799e987d486567
Kayne <93083042+Kayne64@users.noreply.github.com>
2022-12-31 11:50:57 +1100

    update: config.lua

--- config.lua
+++ config.lua
@@ -2,6 +2,8 @@ config = {}
 
 config.esx = false                        -- If you are using esx this will enable player names (esx_identity required)
 
+config.qbcore = false -- If you are using QBCore this will use the Character Name instead of the Steam Name
+
 config.connectionMessages = true         -- set this to true if you would like join and leave messages
 
 config.DiscordWebhook = false -- Set to your Discord Webhook URL 
