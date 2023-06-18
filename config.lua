1d293ff12a9c3e5169e570698cd3682df4bc0c04
OrIoN7531 <68707187+OrIoN7531@users.noreply.github.com>
2023-06-18 16:18:06 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -9,3 +9,14 @@ config.connectionMessages = true          -- set this to true if you would like 
 config.antiSpam = true                    -- set this to true if you would like to use the cc chat antispam system                      
 
 config.DiscordWebhook = false             -- Set to your Discord Webhook URL 
+
+config.emoji = {
+    chatMessage = true, -- enable emojis for text (ooc)
+    ooc = true, -- enable emojis for /ooc
+    me = true, --  enable emojis for /me
+    doo = true, --  enable emojis for /do
+    news = true, --  enable emojis for /news
+    ad = true, --  enable emojis for /ad
+    twt = true, --  enable emojis for /twt
+    anon = true, --  enable emojis for /anon
+}
